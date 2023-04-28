9943e1cbb7bd2812dee3a64af1b63f82907de711
Andreaswiv <andreaswiv@uia.no>
2023-04-28 11:48:25 +0000

    endringer

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Andreaswiv/sem03simpleclient.git
+
+go 1.18
+
+require github.com/Andreaswiv/is105sem03 v0.0.0-20230428113401-79588bd30feb
--- tcp_client.go
+++ tcp_client.go
@@ -1,28 +1,60 @@
 package main
 
 import (
-	"net"
-	"log"
-	"os"
+    "log"
+    "net"
+    "os"
+    "strings"
+
+    "github.com/Andreaswiv/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
-	if err != nil {
-		log.Fatal(err)
-	}
-    
-	log.Println("os.Args[1] = ", os.Args[1])
+    conn, err := net.Dial("tcp", "172.17.0.3:8999")
+    if err != nil {
+        log.Fatal(err)
+    }
 
- 	_, err = conn.Write([]byte(os.Args[1]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+    log.Println("os.Args[1] = ", os.Args[1])
+
+    var response string
+
+    if strings.ToLower(os.Args[1]) == "ping" {
+        _, err = conn.Write([]byte("ping"))
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        buf := make([]byte, 1024)
+        n, err := conn.Read(buf)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        response = string(buf[:n])
+    } else {
+        kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+        log.Println("Kryptert melding: ", string(kryptertMelding))
+
+        _, err = conn.Write([]byte(string(kryptertMelding)))
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        buf := make([]byte, 1024)
+        n, err := conn.Read(buf)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        kryptertSvar := string(buf[:n])
+        log.Println("Dekryptert svar: ", kryptertSvar)
+
+        dekryptertSvar := mycrypt.Krypter([]rune(kryptertSvar), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+        log.Println("Dekryptert svar: ", string(dekryptertSvar))
+
+        response = string(dekryptertSvar)
+    }
+
+    log.Println("Svar fra serveren: ", response)
 }
